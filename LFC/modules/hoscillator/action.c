1bbc42b265e40e72a9d79831a5cf65e4ed901f27
Edoardo Bertoli <bertoliedoardo99@gmail.com>
2023-11-18 22:39:20 +0100

    Update action.c

--- LFC/modules/hoscillator/action.c
+++ LFC/modules/hoscillator/action.c
@@ -30,7 +30,7 @@ double action_dbl ()
 	for(i = 0; i < N - 1; i++)
 	{
 		sum += 0.5 * (xx[i+1] - xx[i])*(xx[i+1] - xx[i]) 
-			+ (OM*OM)*0.25*(xx[i+1]*xx[i+1] + xx[i]*xx[i]);
+		       + (OM*OM)*0.25*(xx[i+1]*xx[i+1] + xx[i]*xx[i]);
 	}
 	sum*= M/(2.0);
 	return sum;
