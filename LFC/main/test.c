39eb17824d3583d22e63030009bd1382136121de
Edoardo Bertoli <bertoliedoardo99@gmail.com>
2023-12-11 18:05:28 +0100

    Added new neovim setup

--- LFC/main/test.c
+++ LFC/main/test.c
@@ -1,65 +1,56 @@
 #define MAIN_PROGRAM
 
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include "global.h"
 #include "action.h"
-#include "random.h"
-#include "start.h" 
-
+#include "global.h"
+/*#include "random.h"
+ *#include <stdlib.h>
+ */
+#include "start.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*
  * TO DO: add vector utility function in a new module
  *        add process time calculation in a util module
- */ 
+ */
 
-
-
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   /*Measuring time*/
   double t1;
   double t2;
-  int i; 
-  FILE* file;
+  int i;
+  FILE *file;
   unsigned int seed;
   int runs;
 
   t1 = (double)clock();
-   
+
   seed = 15031999;
-  /* Initialize random generator */ 
+  /* Initialize random generator */
   start_ranlux(1, seed);
-  
+
   /*
   WARM START
   ranlxd(xx, N);
-  */ 
-
+  */
 
   /*COLD START*/
-  for(i = 0; i < N; i++)
-  {
+  for (i = 0; i < N; i++) {
     xx[i] = 0.0;
   }
 
+  file = fopen("./plot/data", "w");
 
-  file = fopen("./plot/data","w");
-  
   runs = 500;
 
-  for(i=0; i < runs; i++)
-  {
-    fprintf(file,"%f,%d\n", action_dbl(xx), i);
+  for (i = 0; i < runs; i++) {
+    fprintf(file, "%f,%d\n", action_dbl(xx), i);
     sweep(xx);
   }
-  fclose(file); 
- 
-  t2 = (double)clock();
-  printf("TIME ELAPSED: %.9e s \n", t2/CLOCKS_PER_SEC - t1/CLOCKS_PER_SEC);
-  return(0);
+  fclose(file);
 
+  t2 = (double)clock();
+  printf("TIME ELAPSED: %.9e s \n", t2 / CLOCKS_PER_SEC - t1 / CLOCKS_PER_SEC);
+  return (0);
 }
